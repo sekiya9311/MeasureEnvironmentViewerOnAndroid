7c919e3b53f4e16f176cefb395943cf864913d65
sekiya9311 <sekiya9311@gmail.com>
2020-03-16 00:00:01 +0900

    refactor

--- app/src/main/java/com/sekiya9311/measureenvironment/feature/environmentgraph/EnvironmentGraphFragment.kt
+++ app/src/main/java/com/sekiya9311/measureenvironment/feature/environmentgraph/EnvironmentGraphFragment.kt
@@ -1,38 +1,18 @@
 package com.sekiya9311.measureenvironment.feature.environmentgraph
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.sekiya9311.measureenvironment.R
+import androidx.fragment.app.viewModels
 import com.sekiya9311.measureenvironment.databinding.FragmentEnvironmentGraphBinding
 
 class EnvironmentGraphFragment : Fragment() {
 
-    private val viewModel by lazy {
-        ViewModelProvider(this).get(EnvironmentGraphViewModel::class.java)
+    private val viewModel: EnvironmentGraphViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentEnvironmentGraphBinding.bind(view)
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        val binding = DataBindingUtil.inflate<FragmentEnvironmentGraphBinding>(
-            inflater,
-            R.layout.fragment_environment_graph,
-            container,
-            false
-        )
-
-        return binding.root
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-    }
-
 }
--- app/src/main/java/com/sekiya9311/measureenvironment/feature/environments/EnvironmentsFragment.kt
+++ app/src/main/java/com/sekiya9311/measureenvironment/feature/environments/EnvironmentsFragment.kt
@@ -1,38 +1,18 @@
 package com.sekiya9311.measureenvironment.feature.environments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.sekiya9311.measureenvironment.R
+import androidx.fragment.app.viewModels
 import com.sekiya9311.measureenvironment.databinding.FragmentEnvironmentsBinding
 
 class EnvironmentsFragment : Fragment() {
 
-    private val viewModel by lazy {
-        ViewModelProvider(this).get(EnvironmentsViewModel::class.java)
+    private val viewModel: EnvironmentsViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentEnvironmentsBinding.bind(view)
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        val binding = DataBindingUtil.inflate<FragmentEnvironmentsBinding>(
-            inflater,
-            R.layout.fragment_environments,
-            container,
-            false
-        )
-
-        return binding.root
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-    }
-
 }
