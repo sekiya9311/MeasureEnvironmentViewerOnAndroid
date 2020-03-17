e8960650f2fc000a90c4dfa77a376d18074ac729
sekiya9311 <sekiya9311@gmail.com>
2020-03-18 00:04:45 +0900

    Add firebase auth logic

--- app/src/main/java/com/sekiya9311/measureenvironment/MainActivity.kt
+++ app/src/main/java/com/sekiya9311/measureenvironment/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.sekiya9311.measureenvironment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.sekiya9311.measureenvironment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +21,59 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
+    private val firebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (firebaseAuth.currentUser == null) {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        val client = GoogleSignIn.getClient(this, gso)
+
+        startActivityForResult(client.signInIntent, RC_SIGN_IN)
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        firebaseAuth
+            .signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (!task.isSuccessful) {
+                    closeApp()
+                    return@addOnCompleteListener
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode != RC_SIGN_IN) return
+
+        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+        try {
+            val account = task.getResult(ApiException::class.java)
+            firebaseAuthWithGoogle(account!!)
+        } catch (e: ApiException) {
+            closeApp()
+        }
+    }
+
+    private fun closeApp() {
+        finishAndRemoveTask()
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 2308
     }
 }
